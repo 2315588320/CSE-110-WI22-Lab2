2659050955251b83d55b371898ce3951f193fca2
Jinming Zhang <jiz083@ucsd.edu>
2022-01-13 13:06:12 -0800

    commit

--- app/src/main/java/edu/ucsd/cse110/calculator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/calculator/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         // There is a bug in this function! One of your exercises is to find it!
 
         // Hello world this is a new commen!
+        // Hello world this is a new commen!
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
